6c20aa7f113d94f78f07475cbfa8e7790b712668
Bea navarro <beanavarro85@gmail.com>
2023-05-22 16:32:00 +0000

    added snpcalling

---
+++ run_bwa_mem.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+if [ "$#" -ne 5 ]; then
+    echo "Usage: $0 reference fastq1 fastq2 output nthreads"
+	exit 2
+fi
+
+
+# Define variables
+REFERENCE=$1
+FASTQ1=$2
+FASTQ2=$3
+OUT=$4
+THREADS=$5
+
+# Check if reference is indexed, and if not, index it
+if [ ! -e "$REFERENCE.bwt" ]; then
+    echo "Indexing reference genome..."
+    bwa index $REFERENCE
+fi
+
+
+# Map reads to reference using BWA-MEM and output SAM file
+RG="@RG\tID:NL\tSM:$OUT\tPL:illumina\tLB:lib1\tPU:unit1"
+bwa mem -t $THREADS -M -R $RG $REFERENCE $FASTQ1 $FASTQ2 | samtools sort -o ${OUT}.sorted.bam -
+
+# Index sorted BAM file${OUT}.sorted.bam
+
+#bamtools index $(basename $FASTQ1 .fastq.gz).sorted.bam
+samtools index ${OUT}.sorted.bam
+
+## reduce bam size
+#reduce_bam.py ${OUT}.sorted.bam
+
---
+++ run_gatk4_haplotypecaller.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+if [ "$#" -ne 2 ]; then
+	    echo "Usage: $0 sample.bam reference.fasta"
+	            exit 2
+fi
+
+
+
+bam=$1
+ref=$2
+
+##### check reference indexes
+
+if [ -f ${ref}.fai ]; then
+	echo "${ref}.fai exists, moving on..."
+else	
+	echo "Generating ${ref}.fai index ..."
+	        samtools faidx $ref
+fi
+
+# Check if reference is indexed, and if not, index it
+if [ -f "$(basename $ref .fasta).dict" ]; then
+	echo "$(basename $ref .fasta).dict exists, moving on..."
+else
+	echo "Creating $ref dictionary..."
+	    picard CreateSequenceDictionary R=$ref O=$(basename $ref .fasta).dict
+fi
+
+
+
+#### Dedup
+#echo "
+picard MarkDuplicates \
+	I=${bam} \
+	O=$(basename $bam .bam).dedup.bam \
+	METRICS_FILE= $(basename $bam .bam).dup_metrics.txt\
+	REMOVE_DUPLICATES=true \
+	VALIDATION_STRINGENCY=LENIENT AS=true 
+
+## Index the Deduped merged bam files
+#picard BuildBamIndex \
+#	I=$(basename $bam .bam).dedup.bam \
+#	VALIDATION_STRINGENCY=LENIENT
+samtools index $(basename $bam .bam).dedup.bam
+## haplotype caller
+gatk --java-options "-Xmx10g" HaplotypeCaller \
+	-R $ref \
+	-I $(basename $bam .bam).dedup.bam \
+	-O $(basename $bam .bam).vcf.gz \
+	-ERC GVCF
+#"
