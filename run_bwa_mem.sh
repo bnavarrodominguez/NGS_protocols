bf721dc95279bc7680ab094e5977d3d19511f38d
Bea navarro <beanavarro85@gmail.com>
2024-01-09 16:03:14 +0000

    update run_bwa_mem

--- run_bwa_mem.sh
+++ run_bwa_mem.sh
@@ -21,7 +21,8 @@ fi
 
 # Map reads to reference using BWA-MEM and output SAM file
 RG="@RG\tID:NL\tSM:$OUT\tPL:illumina\tLB:lib1\tPU:unit1"
-bwa mem -t $THREADS -M -R $RG $REFERENCE $FASTQ1 $FASTQ2 | samtools sort -o ${OUT}.sorted.bam -
+#bwa mem -t $THREADS -M -R $RG $REFERENCE $FASTQ1 $FASTQ2 | samtools sort -o ${OUT}.sorted.bam -
+bwa mem -t $THREADS -M -R $RG $REFERENCE $FASTQ1 $FASTQ2 | samtools sort - -o ${OUT}.sorted.bam
 
 # Index sorted BAM file${OUT}.sorted.bam
 
